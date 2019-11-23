5947406ecd5c96e0e4d10e83b10ee75e83033954
Hsin-Hung Lin <linil2j@gmail.com>
2019-11-23 11:56:39 +0800

    Add `compare_selected_benchmark_logs.sh`

---
+++ compare_selected_benchmark_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# comapres all logs in current directory.
+#BM_LIST=('full_str_int' 'filtered_str_int' 'cvc4-str-term' 'cvc4-str-pred' 'stringfuzz' 'str_2' 'slog' 'Leetcode' 'PyEx_unsat' 'PyEx_sat' 'PyEx_todo' 'Kaluza_unsat' 'Kaluza_sat' 'Kaluza_todo')
+BM_LIST=('full_str_int' 'cvc4-str-term' 'cvc4-str-pred' 'stringfuzz' 'str_2' 'Leetcode' 'PyEx_unsat' 'PyEx_sat' 'PyEx_todo')
+
+#echo "benchmarks to be checked: " ${BM_LIST[*]}
+#read -n 1 -s -r -p "tool_name = ${1}... Press any key to start."
+
+for bm in ${BM_LIST[*]}
+do
+    compare_benchmark_logs ${bm}
+done
